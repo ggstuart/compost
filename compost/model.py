c9b379b7fb1ea570bf073e75a15ab58fceeb7b00
Graeme Stuart <ggstuart@dmu.ac.uk>
2015-07-06 18:03:16 +0100

    bugfix join must have named series - version bump

--- compost/model.py
+++ compost/model.py
@@ -15,6 +15,7 @@ class WeekdayAverageModel(object):
         data = Dataset(source_data, 60*60*24, cumulative).interpolate()
         source = data.measurements.diff().value[1:].groupby(data.measurements[1:].index.weekday)
         self.parameters = source.mean()
+        self.parameters.name = ['value']
 
     def prediction(self, index):
         return DataFrame(index=index).join(self.parameters, on=index.weekday).value
@@ -25,6 +26,7 @@ class MonthlyAverageModel(object):
         data = Dataset(source_data, 60*60*24, cumulative).interpolate()
         source = data.measurements.diff().value[1:].groupby(data.measurements[1:].index.month)
         self.parameters = source.mean()
+        self.parameters.name = ['value']
 
     def prediction(self, index):
         return DataFrame(index=index).join(self.parameters, on=index.month).value
